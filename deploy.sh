8fbb2db37b07672ad57ddf00f3b32340d620f05b
fanlun008 <51396450+fanlun008@users.noreply.github.com>
2019-07-29 17:45:36 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
-export BUILD_ID=dontKillMe
 pid=$(netstat -nlp | grep :8088 | awk '{print $7}' | awk -F"/" '{ print $1 }');
 if [ -n "${pid}" ]; then
 kill -9 ${pid};
 fi
-JENKINS_NODE_COOKIE=dontKillMe nohup java -jar /opt/parking-tag-backend/parking-tag-1.0-SNAPSHOT.jar &
+nohup java -jar /opt/parking-tag-backend/parking-tag-1.0-SNAPSHOT.jar &
