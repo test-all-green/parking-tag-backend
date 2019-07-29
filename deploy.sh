7e064abc61ee6b6dfdf6d48e74924b0c8028b3f5
fanlun008 <51396450+fanlun008@users.noreply.github.com>
2019-07-29 18:00:52 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ pid=$(netstat -nlp | grep :8088 | awk '{print $7}' | awk -F"/" '{ print $1 }');
 if [ -n "${pid}" ]; then
 kill -9 ${pid};
 fi
-nohup java -jar /opt/parking-tag-backend/parking-tag-1.0-SNAPSHOT.jar &
+nohup java -jar /opt/parking-tag-backend/parking-tag-1.0-SNAPSHOT.jar > /opt/parking-tag-backend/logs/application.log 2>&1 &
