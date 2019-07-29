6d832797d98ffcab1b51baf18d4a8b5b908c0fbb
CyrilYao <cyril.yao@oocl.com>
2019-07-29 16:18:35 +0800

    feat:add enum of order status and work status

---
+++ src/main/java/cn/tag/enums/OrderStatusEnum.java
@@ -0,0 +1,40 @@
+package cn.tag.enums;
+
+public enum OrderStatusEnum {
+
+    PARKING_WAIT("PW","等待停车受理"),
+    PARKING_ING("PI","停车中"),
+    HAVE_PARKING("HP","已停车"),
+    FETCH_WAIT("FW","等待取车受理"),
+    FETCH_ING("FI","取车中"),
+    GO_ING("GI","前往取车地点"),
+    PAY_WAIT("PW","待支付"),
+    FINISH("F","已完成"),
+    CANCEL("C","取消订单");
+
+    private String description;
+
+    private String key;
+
+
+    OrderStatusEnum(String key, String description) {
+        this.description = description;
+        this.key = key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
