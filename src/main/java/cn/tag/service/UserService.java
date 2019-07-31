f0671d531719c590b52037d1aca0beb383c284cf
CyrilYao <cyril.yao@oocl.com>
2019-07-31 08:26:56 +0800

    fix: throw exception when register with an exist phone

--- src/main/java/cn/tag/service/UserService.java
+++ src/main/java/cn/tag/service/UserService.java
@@ -1,16 +1,23 @@
 package cn.tag.service;
 
 import cn.tag.entity.User;
+import cn.tag.exception.CustomException;
 import cn.tag.respository.UserRepository;
 import cn.tag.util.SHA1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
     public User register(User user) {
+        List<User> userRepositoryByPhone = userRepository.findByPhone(user.getPhone());
+        if(userRepositoryByPhone.size() > 0){
+            throw new RuntimeException("输入的手机号码已被注册！");
+        }
         user.setStatus(0);
         user.setUserPassword(SHA1.encode(user.getUserPassword()));
         return userRepository.saveAndFlush(user);
